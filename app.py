75d77dc175277a8bff48c60bfdb9faf545f1e370
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2023-09-18 21:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
 def homepage():
-    return render_template("index.html")
+    return render_template("result.html")
 
 @app.route("/review" , methods = ['POST' , 'GET'])
 def index():
